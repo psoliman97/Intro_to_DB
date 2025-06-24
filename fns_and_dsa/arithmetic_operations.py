e9ca9196b3af016573be6349cda84bf9fcf6af9f
Peter Soliman <petersoliman94@gmail.com>
2025-06-24 05:23:03 +0300

    adding

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,13 @@
-def perform_operation(num1, num2, operation) : 
-    if operation == 'add':
+def perform_operation(num1, num2, operation):
+    if operation == "add":
         return num1 + num2
-    elif operation == 'subtract':
+    elif operation == "subtract":
         return num1 - num2
-    elif operation == 'multiply':
+    elif operation == "multiply":
         return num1 * num2
-    elif operation == 'divide':
+    elif operation == "divide":
         if num2 == 0:
-            return "Error: Division by zero"
+            return "Error: Division by zero is not allowed."
         return num1 / num2
     else:
-        return "Error: Unsupported operation"
+        return "Error: Invalid operation."
