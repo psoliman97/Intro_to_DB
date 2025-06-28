a205eaa22e394ee11494de1fd26262ddfbc525af
Peter Soliman <petersoliman94@gmail.com>
2025-06-28 22:47:54 +0300

    updatinf

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount ():
+    def __init__(self,intial_balance=0) :
+        self.__account_balanc = intial_balance
+
+    def deposite(self, amount) :
+        if amount > 0 :
+            self.__account_balanc += amount
+
+    def withdrow (self, amount) :
+        if amount > 0 and self.__account_balanc >= amount:
+            self.__account_balanc -= amount
+            return True
+        return False
+    
+    def display_balance (self) :
+        print(f"Current Balance : {self.__account_balanc}")
+
+
+        
