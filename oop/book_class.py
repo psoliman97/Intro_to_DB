49937867f92c24a3ae5a0604e7ab3c0368d39d0d
Peter Soliman <petersoliman94@gmail.com>
2025-07-06 21:53:50 +0300

    update

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,13 +4,13 @@ class Book:
         self.author = author
         self.year = year
 
-    def __del__ (self):
+    def __del__(self):
         print(f"Deleing : {self.title}")
     
     def __str__(self):
         return f"{self.title} is written by {self.author} at {self.year}"
     
-    def __repr__(self) :
+    def __repr__(self):
         return f"Book ('{self.title}','{self.author}', '{self.year}') "
         
         
